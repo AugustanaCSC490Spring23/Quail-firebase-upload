cebf751c124c6c0f41307b27ea0c0dc766bd3885
Christopher Le <locvicvn1234@gmail.com>
2023-03-03 12:45:44 -0600

    initial commit

---
+++ firebase-upload-tutorial/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  ref,
+  uploadBytes,
+  getStorage,
+  listAll,
+  list,
+  getDownloadURL,
+} from "@firebase/storage";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState("");
+  const [filename, setFilename] = useState("");
+  const [file, setFile] = useState(null);
+  const storage = getStorage();
+
+
+  const handlePictureChange = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      setImageUrl(reader.result);
+    };
+    reader.readAsDataURL(file);
+    setFilename(file.name);
+    setFile(file);
+  };
+
+  const uploadPhoto = () => {
+    const filenameRef = new Date().getTime() + "-" + filename;
+    const storageRef = ref(
+      storage,
+      `quail_firebase_upload_tutorial/images/${filenameRef}`
+    );
+    
+    if (file) {
+      uploadBytes(storageRef, file)
+      .then((snapshot) => {
+        console.log("Uploaded an image!");
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error uploading image:", error);
+      });
+    } else {
+      alert("Please select a file to upload!");
+    }
+  };
+  return (
+    <div className="App">
+       Choose a picture:
+      <br />
+      <input type="file"  accept="image/png, image/jpeg, image/heic, image/jpg" onChange={handlePictureChange} />
+      <br />
+      <button onClick={() => uploadPhoto()}> Upload photo </button>
+    </div>
+  );
+}
+
+export default App;
